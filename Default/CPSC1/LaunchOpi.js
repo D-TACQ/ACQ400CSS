3d14901df26e524e45e8e112ca1a09e4bb91cc7d
Peter Milne <peter.milne@d-tacq.com>
2012-05-11 09:47:15 +0100

    Initial CPSC version

---
+++ Default/CPSC1/LaunchOpi.js
@@ -0,0 +1,16 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var flagName = "firstRun";
+//Avoid running this script if the script is triggered during opi startup.
+if(widgetController.getExternalObject(flagName) == null){
+	widgetController.setExternalObject(flagName, false);	
+}else{
+
+	var macroInput = DataUtil.createMacrosInput(true);
+	macroInput.put("UUT", PVUtil.getString(pvArray[0]));		
+	
+	//Open an OPI with the new Macro Input.
+       var opi = widget.getPropertyValue("name") + ".opi";
+	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);
+
+}
---
+++ Default/CPSC1/ToggleTrigger.js
@@ -0,0 +1,20 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+importPackage(Packages.org.eclipse.jface.dialogs);
+
+var flagName = "firstRun";
+//Avoid running this script if the script is triggered during opi startup.
+if(widgetController.getExternalObject(flagName) == null){
+	widgetController.setExternalObject(flagName, false);	
+}else{
+	var trigger;
+	
+	if (PVUtil.getLong(pvArray[0]) == 1){
+		var old = PVUtil.getLong(pvArray[1]);
+		if (old != 0){
+			old = 0;
+		}else{
+			old = 1;
+		}		
+		pvArray[1].setValue(old);
+	}
+}
