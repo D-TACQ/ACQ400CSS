65294b355517b4efedebb908861d5171ca694258
Peter Milne <peter.milne@d-tacq.com>
2013-09-12 12:44:31 +0100

    works with multiple sites on acq2006. well, at least site1 tested 	modified:   LaunchOpi.js 	modified:   LaunchOpi8.js 	modified:   acq420_launcher.opi

--- ACQ420FMC/LaunchOpi.js
+++ ACQ420FMC/LaunchOpi.js
@@ -6,11 +6,9 @@ if(widgetController.getExternalObject(flagName) == null){
 	widgetController.setExternalObject(flagName, false);	
 }else{
 	var macroInput = DataUtil.createMacrosInput(true);
-	macroInput.put("UUT", PVUtil.getString(pvArray[0]));	
-	// not easy to get strings "0", "1" etc to stay as strings...	
-	macroInput.put("SITE", "0" ); // PVUtil.getString(pvArray[1]));
-	//Open an OPI with the new Macro Input.
-       var opi = widget.getPropertyValue("name") + ".opi";
+	macroInput.put("UUT", PVUtil.getString(pvArray[0]));		
+	macroInput.put("SITE", PVUtil.getLong(pvArray[1]));
+		
+    var opi = widget.getPropertyValue("name") + ".opi";
 	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);
-
 }
--- ACQ420FMC/LaunchOpi8.js
+++ ACQ420FMC/LaunchOpi8.js
@@ -16,18 +16,14 @@ if(widgetController.getExternalObject(flagName) == null){
 	widgetController.setExternalObject(flagName, false);	
 }else{
 	var macroInput = DataUtil.createMacrosInput(true);
-	macroInput.put("UUT", PVUtil.getString(pvArray[0]));	
-	// not easy to get strings "0", "1" etc to stay as strings...	
-	macroInput.put("SITE", "0" ); // PVUtil.getString(pvArray[1]));
-	//Open an OPI with the new Macro Input.
-       var opi = widget.getPropertyValue("name") + ".opi";
+	macroInput.put("UUT", PVUtil.getString(pvArray[0]));
+	macroInput.put("SITE", PVUtil.getLong(pvArray[1]));	
        
 	var ch = PVUtil.getLong(pvArray[2]);
 	for (var nc = 1; nc <= 8; ++nc, ++ch){
-		macroInput.put("CH"+pad(nc, 2), pad(ch, 2));
-		//macroInput.put("CH"+pad(nc, 2), pad(ch%4 + 1, 2)); 		
+		macroInput.put("CH"+pad(nc, 2), pad(ch, 2));		 		
 	}
 	       
+	var opi = widget.getPropertyValue("name") + ".opi";	       
 	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);
-
 }
