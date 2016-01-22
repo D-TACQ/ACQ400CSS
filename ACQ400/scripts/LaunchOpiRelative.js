990e8db666420c634aec99aab6151cb7ddb58fc0
Peter Milne <peter.milne@d-tacq.com>
2016-01-22 15:03:26 +0000

    missing link to make BOLO MIMIC work 	new file:   ACQ400/scripts/LaunchOpiRelative.js

---
+++ ACQ400/scripts/LaunchOpiRelative.js
@@ -0,0 +1,14 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+var flagName = "firstRun";
+//Avoid running this script if the script is triggered during opi startup.
+if(widgetController.getExternalObject(flagName) == null){
+	widgetController.setExternalObject(flagName, false);	
+}else{
+	var macroInput = DataUtil.createMacrosInput(true);
+	macroInput.put("UUT", PVUtil.getString(pvArray[0]));		
+	macroInput.put("SITE", PVUtil.getLong(pvArray[1]));
+		
+	var opi = widget.getPropertyValue("name") + ".opi";
+	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);
+}
