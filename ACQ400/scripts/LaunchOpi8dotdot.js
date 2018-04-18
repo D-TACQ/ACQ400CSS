310c30a4292227d38500a07b193d57975c77cd1e
Peter Milne <petermilne@users.noreply.github.com>
2018-04-18 12:51:18 +0100

    add display8 button to each uut line add global "capture" button
    	modified:   ACQ400/opi/stack_plot8volts_transient.opi
    	modified:   ACQ400/opi/transient_state.opi
    	modified:   ACQ400/opi/transient_state_8.opi
    	new file:   ACQ400/scripts/LaunchOpi8dotdot.js

---
+++ ACQ400/scripts/LaunchOpi8dotdot.js
@@ -0,0 +1,49 @@
+importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
+
+function pad(ii, nchars)
+{
+	if (nchars == 2 && ii < 10){
+		return "0"+ii;
+	}else{
+		return ""+ii;
+	}
+}
+
+
+var flagName = "firstRun";
+//Avoid running this script if the script is triggered during opi startup.
+if(widgetController.getExternalObject(flagName) == null){
+	widgetController.setExternalObject(flagName, false);	
+}else{
+	var macroInput = DataUtil.createMacrosInput(true);
+	macroInput.put("UUT", PVUtil.getString(pvArray[0]));
+	macroInput.put("SITE", PVUtil.getLong(pvArray[1]));	
+       
+	var ch = PVUtil.getLong(pvArray[2]);
+	var chx = "CH";
+	for (var nc = 1; nc <= 8; ++nc, ++ch){
+		var ch0x = pad(ch, 2);
+		macroInput.put("CH"+pad(nc, 2), ch0x);
+		if (nc == 1){
+			chx += ch0x;
+		}
+		if (nc == 8){
+			chx += ":" + ch0x;
+		}		 		
+	}
+	
+	var plot_time = 0;
+	if (pvArray[3] != null){
+		plot_time = PVUtil.getLong(pvArray[3]);
+	}
+	/* plot_time != 0, use embedded tbx */
+	macroInput.put("tbx", plot_time != 0? "$(TB)": "");
+	macroInput.put("xtitle", plot_time==0? "Samples": plot_time == 2? "Hz": "Seconds"); 
+	macroInput.put("CHX", chx);
+	       
+	var opibase = "" + widget.getPropertyValue("name");
+	var name_ext = opibase.indexOf("__");
+	if (name_ext > 0) opibase = opibase.substr(0, name_ext);
+	var opi = "" + opibase + ".opi";	       
+	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);	
+}
