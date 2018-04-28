73ef3f7de842a343e6d8f16347c44226059542aa
Peter Milne <peter.milne@d-tacq.com>
2018-04-28 16:27:00 +0100

    acq2106clktree : better mimic for ACQ480 usage traces clock from end to end, showing decimation calulations BUG: SIG:CLK_S1:FREQ only good to 40MHz, use SIG:CLK_MB:FREQ instead 	modified:   ACQ400/opi/acq2106_clktree_site_480.opi 	modified:   ACQ400/opi/acq2106clktree.opi 	modified:   ACQ400/opi/sitemasterclk.opi 	modified:   ACQ400/scripts/show480.js

--- ACQ400/scripts/show480.js
+++ ACQ400/scripts/show480.js
@@ -2,13 +2,13 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 var sitelist = PVUtil.getString(pvArray[0]);
 var dx = PVUtil.getLong(pvArray[1]);
-
+var acq480_true = PVUtil.getLong(pvArray[2]) != 0? true: false;
 var site=dx-1;
 
 var sites = sitelist.split(",");
 var isVisible = false;
 if (site < sites.length){
-	isVisible = sites[site].search("48[02]") >-1? true: false;
+	isVisible = sites[site].search("48[02]") >-1? acq480_true: !acq480_true;
 }
 
 widget.setPropertyValue("visible", isVisible);
