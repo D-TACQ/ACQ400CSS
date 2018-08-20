d1a960c4dc7b2c8c5f4043fd2866de287e5019f7
Peter Milne <peter.milne@d-tacq.com>
2018-08-20 18:21:45 +0100

    BF: works with sparse sites eg 1,3,5 REMOVED CLK_DIST mux between Si5326 and Si53315 	modified:   ACQ400/opi/acq1001clktree.opi 	modified:   ACQ400/opi/acq2106clktree.opi 	modified:   ACQ400/scripts/show480.js 	modified:   ACQ400/scripts/showIfSiteMatch.js

--- ACQ400/scripts/show480.js
+++ ACQ400/scripts/show480.js
@@ -7,6 +7,14 @@ var site=dx-1;
 
 var sites = sitelist.split(",");
 var isVisible = false;
+
+for (var kv in sites) {
+	var kva = sites[kv].split("=");
+	if (site == kva[0]){
+    	isVisible = kva[1].search("48[02]") >-1? acq480_true: !acq480_true;
+	    break;
+	}
+}
 if (site < sites.length){
 	isVisible = sites[site].search("48[02]") >-1? acq480_true: !acq480_true;
 }
--- ACQ400/scripts/showIfSiteMatch.js
+++ ACQ400/scripts/showIfSiteMatch.js
@@ -2,14 +2,22 @@ importPackage(Packages.org.csstudio.opibuilder.scriptUtil);
 
 var sitelist = PVUtil.getString(pvArray[0]);
 var dx = PVUtil.getLong(pvArray[1]);
+//loc://clktree_site_${SITE}x("[0-9]")
 var re = new RegExp(PVUtil.getString(pvArray[2]), 'gm');
 
 var site=dx-1;
-
+// sitelist typ 21637,1=423,3=423,5=423
 var sites = sitelist.split(",");
 var isVisible = false;
-if (site < sites.length){
-	isVisible = sites[site].search(re) >-1? true: false;
+
+for (var kv in sites) {
+	var kva = sites[kv].split("=");
+	if (site == kva[0]){
+	    if (kva[1].search(re) > -1){
+	    	isVisible = true;	    	
+	    }
+	    break;
+	}
 }
 
 widget.setPropertyValue("visible", isVisible);
