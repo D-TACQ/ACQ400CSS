aaca5ba34b5039f33f8aa48b1495c7f46c0764bf
Peter Milne <peter.milne@d-tacq.com>
2018-12-02 11:58:00 +0000

    run shot now includes --fclk and --store added a dummy View.. button. Button row is Full! 	new file:   ACQ400/icons/appointment-new.png 	new file:   ACQ400/icons/go-bottom.png 	new file:   ACQ400/icons/go-down.png 	new file:   ACQ400/icons/go-first.png 	new file:   ACQ400/icons/go-last.png 	new file:   ACQ400/icons/go-next.png 	new file:   ACQ400/icons/go-top.png 	new file:   ACQ400/icons/go-up.png 	new file:   ACQ400/icons/help-contents.png 	new file:   ACQ400/icons/insert-image.png 	new file:   ACQ400/icons/media-eject.png 	new file:   ACQ400/icons/system-shutdown-panel.png 	modified:   ACQ400/opi/run_stream.opi 	modified:   ACQ400/opi/stream_state_8.opi 	modified:   ACQ400/scripts/LaunchOpiSecs.js 	modified:   ACQ400/stream8_launcher.opi

--- ACQ400/scripts/LaunchOpiSecs.js
+++ ACQ400/scripts/LaunchOpiSecs.js
@@ -7,7 +7,9 @@ if(widgetController.getExternalObject(flagName) == null){
 }else{
 	var macroInput = DataUtil.createMacrosInput(true);
 	macroInput.put("SECS", PVUtil.getLong(pvArray[0]));
-	macroInput.put("ETRG", PVUtil.getLong(pvArray[1]));	
+	macroInput.put("ETRG", PVUtil.getLong(pvArray[1]));
+	macroInput.put("FCLK", PVUtil.getString(pvArray[2]));
+	macroInput.put("STORE", PVUtil.getLong(pvArray[3]));		
 	var opi = "" + widget.getPropertyValue("name") + ".opi";
     
 	ScriptUtil.openOPI(widgetController,  opi, 0, macroInput);
