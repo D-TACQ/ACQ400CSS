8f3a2f6e55a3b6e68764f0f1161fe5686d6b6071
Peter Milne <peter.milne@d-tacq.com>
2019-03-03 18:54:54 +0000

    THOMSON transient with 4 uuts. Rework transient8 to be more general Install this macro file in your workspace: workspace/thomson.properties (exact path in top of file) Then run cs-studio and customize MACROS to suit 	modified:   opi/transient_state.opi 	modified:   opi/transient_state_8.opi 	modified:   scripts/LaunchOpi8dotdot.js 	modified:   transient8_launcher.opi 	new file:   workspace/thomson.properties

--- ACQ400/scripts/LaunchOpi8dotdot.js
+++ ACQ400/scripts/LaunchOpi8dotdot.js
@@ -18,17 +18,19 @@ if(widgetController.getExternalObject(flagName) == null){
 	var macroInput = DataUtil.createMacrosInput(true);
 	macroInput.put("UUT", PVUtil.getString(pvArray[0]));
 	macroInput.put("SITE", PVUtil.getLong(pvArray[1]));	
-       
-	var ch = PVUtil.getLong(pvArray[2]);
-	var chx = "CH";
-	for (var nc = 1; nc <= 8; ++nc, ++ch){
-		var ch0x = pad(ch, 2);
-		macroInput.put("CH"+pad(nc, 2), ch0x);
-		if (nc == 1){
-			chx += ch0x;
-		}
-		if (nc == 8){
-			chx += ":" + ch0x;
+     
+    if (pvArray[3] != null) {  
+		var ch = PVUtil.getLong(pvArray[2]);
+		var chx = "CH";
+		for (var nc = 1; nc <= 8; ++nc, ++ch){
+			var ch0x = pad(ch, 2);
+			macroInput.put("CH"+pad(nc, 2), ch0x);
+			if (nc == 1){
+				chx += ch0x;
+			}
+			if (nc == 8){
+				chx += ":" + ch0x;
+			}
 		}		 		
 	}
 	
