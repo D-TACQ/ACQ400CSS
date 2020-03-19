d92b4e0f35532dd28bd17b8094cd30988fd1e16a
Peter Milne <peter.milne@d-tacq.com>
2020-03-19 21:49:47 +0000

    selects on ACQ481 as well as ACQ480, ACQ482)  On branch master  Changes to be committed: 	modified:   ACQ400/scripts/show480.js

--- ACQ400/scripts/show480.js
+++ ACQ400/scripts/show480.js
@@ -11,12 +11,12 @@ var isVisible = false;
 for (var kv in sites) {
 	var kva = sites[kv].split("=");
 	if (site == kva[0]){
-    	isVisible = kva[1].search("48[02]") >-1? acq480_true: !acq480_true;
+    	isVisible = kva[1].search("48[012]") >-1? acq480_true: !acq480_true;
 	    break;
 	}
 }
 if (site < sites.length){
-	isVisible = sites[site].search("48[02]") >-1? acq480_true: !acq480_true;
+	isVisible = sites[site].search("48[012]") >-1? acq480_true: !acq480_true;
 }
 
 widget.setPropertyValue("visible", isVisible);
